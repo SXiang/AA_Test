2984364c6de6c2f48d98667dc12454a84c95abe1
Steven Xiang <steven_xiang@acl.com>
2013-08-21 16:55:08 -0700

    TAF_V10 added with test examples

--- src/main/java/ax/keyword/restapi/KeywordExample.java
+++ src/main/java/ax/keyword/restapi/KeywordExample.java
@@ -1,134 +1,132 @@
+/**
+ * 
+ */
 package ax.keyword.restapi;
 
+import com.acl.qa.taf.helper.Interface.KeywordInterface;
+import com.acl.qa.taf.util.FormatHtmlReport;
+import com.acl.qa.taf.util.UTF8Control;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import ax.lib.KeywordTaskHelper;
 
-public class KeywordExample extends KeywordTaskHelper{
-		/**
-		 * Script Name   : <b>KeywordExample</b>
-		 * Generated     : <b>Mar 24, 2012 3:42:42 PM</b>
-		 * Description   : Functional Test Script
-		 * 
-		 * @since  2012/03/24
-		 * @author Steven_Xiang
-		 */
-	    //*********************** Shared Variables in KeywordHelper *******************
-		//***  dpOpenProject
-	    //***  dpEndWith
-	    //***  dpProjectName
-	    //***  dpUnicodeTest
-	    //
-	    
-	  //*********************** Shared Main test Variables  in YourKeywordHelper ***********
-	  //***  dpFields;  
-	  //***  dpExpression; 
-	  //***  dpIf;     
-	  //***  dpFileName; 
-	  //***  dpUseOutputTable; 
-	  //***  dpAppendToFile;
-	  //***  dpSaveLocalOrServer="TBD"; 
-      //***  ...
+import ax.lib.KeywordHelper;
 
+  //Script Name is required for generating keyword doc
+/**
+ * Script Name   : <b>KeywordExample</b>
+ * Generated     : <b>2013-08-16 12:07:24 PM</b> 
+ * Description   : <b>ACL Test Automation</b>
+ * 
+ * @since  2013/08/16
+ * @author Steven_Xiang
+ * 
+ */
+public class KeywordExample extends KeywordHelper implements KeywordInterface {
 
-		// BEGIN of datapool variables declaration   	
-		protected String dpAgingPeriods; //@arg value for Aging Periods
-		                                  //@value = [num1 num2 num3...]
-		protected String dpOn;      //@arg field or expression for test
-		protected String dpCutoffDate; //@arg cutoff date
-		                                //@value dd-MMM-yyyy
+	/**
+	 * @param args
+	 */
 
-		protected String dpSuppressOthers; //@arg 'Yes' or 'No', default to 'No'
-		protected String dpBreak; //@arg field name to be broken
+	//***************  Part 1  *******************
+	// ******* Declaration of variables **********
+	// *******************************************
+	
+	         // Comments in section 'BEGIN' TO 'END' are used for generating keyword docs
+	// BEGIN of datapool variables declaration
+	protected String dpWebDriver; //@arg Selenium webdriver type
+	//@value = HtmlUnit|Firefox|...
 
-		// END of datapool variables declaration
+protected String dpUserName; //@arg username for login
+protected String dpPassword; //@arg password for login
+protected String dpEndWith; //@arg actions after test
+//@value = logout|quit|kill, or empty
+	protected String dpURL; //@arg the url to be tested
+	protected String dpScope; //@arg scope of the project
+							  //@value = working|library, or empty
+	// END of datapool variables declaration
 
-		@Override
-		public boolean dataInitialization() {
-			boolean done= true;
-         
-		//*** Possible shared variables defined in YourKeywordHelper
-//			defaultMenu = "Analyze";
-//			command = "Age";
-//			winTitle = command;
-//			tabMainName = winTitle; //"_Main";
-//			fileExt = ".TXT";
-			
-		//*** Possible methods in YourKeywordHelper
-			//readSharedTestData();
-	       // readSharedMainTestData();        
-	       // readOutputTestData();
-	        
-	   //*** read in data     
-//	        dpCutoffDate = getDpString("CutoffDate");
-//	        dpAgingPeriods = getDpString("AgingPeriods");
-//	        dpOn = getACLFields("On");
-//	       
-//	        dpSuppressOthers = getDpString("SuppressOthers");
-//	        dpBreak = getDpString("Break");
-			return done;
+	@Override
+	public boolean dataInitialization() {
+		super.dataInitialization();
+				
+		dpURL = getDpString("URL");
+		dpScope = getDpString("Scope");	
+		if(!dpScope.equals("")){
+			dpURL += "?scope="+dpScope;
 		}
+		return true;
+	}
 
-		public void testMain(Object[] args) 
-		{
-//			super.testMain(args);
-		//*** your test logic
-//			openTest();
-//			aclMainDialog();        
-//		    aclEndWith("fileAction");  
-//		    doVerification(dpTo); //"Log"
-//			aRou.exeACLCommands(dpPostCmd);	
-//			
-		}
+	
+	//***************  Part 2  *******************
+	// *********** Test logic ********************
+	// *******************************************
+	
+	//Comments - 'Steps' in this part are used to generate keyword docs
+	@Override
+	public void testMain(Object[] args){			
+		super.testMain(onInitialize(args, getClass().getName()));
 		
-		public void aclMainDialog(){
-//			mainDialog = mainDialog();//new TopLevelSubitemTestObject(findTopLevelWindow(winTitle)) ;
-//			
-//			click(findPagetab(mainDialog, mainTab), mainTab);	
-//	    	tabMain = findSubWindow(mainDialog,true,tabMainName);
-//			thisMainTab(tabMain);
-//
-//			click(findPagetab(mainDialog, outputTab), outputTab);	
-//	    	tabOutput = findSubWindow(mainDialog,true,tabOutputName);    	
-//			thisOutputTab(tabOutput);
-//			
-//			click(findPagetab(mainDialog, moreTab), moreTab);
-//	    	tabMore = findSubWindow(mainDialog,true,tabMoreName);
-//			thisMoreTab(tabMore);
-//			
-		}
+		//Steps:
+		//@Step Start or activate AUT - Browser based on the value of 'WebDriver'
+		//@Step Handled browser security warning if there is one
+		//@Step Check user CAS session, report info or error accordingly.
+		//@Step If login required, submit username and password to CAS
+		//@@Step Fill in 'username' and 'password'
+		//@@Step Submit
+		//@@Step Verify page status and report accordingly
+		//@Step Get page contents for verification
+		//@@Step Compare actual contents with expected data - pre saved data or prepared master data
+		//@@Step Report errors by indicating the actual line number and the contents
+		//@@Step Take a screenshot if anything wrong (it happens whenever there is a warring or error reported)
+		//@Step Execute the command specified in 'EndWith' such as 'Kill','Close' or 'Logout' 
 		
-		public void thisMainTab(WebElement tabDialog){
-//			String keyToOnExp = winTitle+" On...";	
-//			String keyToExp = "Subtotal Fields...";
-//			
-//			// Work on tabDialog
-//			if(!dpOn.equals("")&&!selectedFromFields(tabDialog,keyToOnExp,dpOn)){
-//				actionOnSelect(findComboBox(tabDialog,true,0),keyToOnExp,dpOn,"New");
-//			}
-//			
-//			if(!dpFields.equals("")&&!selectedFromFields(tabDialog,keyToExp,dpFields)){
-//				selectSomeFields(findTable(tabDialog,true,0),dpFields);
-//			}
-//			
-//            .....
-		}
+		casLogin(dpURL);
+		doVerification();
+		cleanUp(dpURL);
 		
-	    public void thisMoreTab(WebElement tabDialog){   	
-//	    	// Work on tabDialog
-//	    	super.thisMoreTab(tabDialog);
-//	    	
-//	    	if(!dpSuppressOthers.equals("")){
-//				actionOnCheckbox(findCheckbutton(tabDialog,"Suppress Others"),
-//						"Suppress Others",
-//						dpSuppressOthers.equalsIgnoreCase("Yes")?true:false,"New");
-//				}
-//	    	
-//	    	if(!dpBreak.equals("")){
-//	  		  actionOnText(findEditbox(tabDialog,true,3),"Break",dpBreak,"New");	
-//	  		}
-//	    	
-	    }
-	    
+	  //*** cleanup by framework ***		
+		onTerminate();
+	}
+	
+	//***************  Part 3  *******************
+	// *** Implementation of test functions ******
+	// *******************************************
+		
+	public void doVerification(){
+		
+		// Set file name and path for verification
+		// they will be extended with [number] suffix at each comparison.
+		
+		setupTestFiles();  
 
-	 }
+		String actualResult = UTF8Control.utf8decode(driver.getPageSource());
+		if(casAuthenticated){
+			logTAFInfo("JSON data: '\n\t\t"+FormatHtmlReport.getHtmlPrintable(actualResult,100)+"...");
+			// compare Json Result - exact master and actual files are handled by framework.
+			logTAFStep("Demo - first file verification");
+			    compareJsonResult(actualResult);
+			// Do another for demo - multiple comparisons - Steven
+			logTAFStep("Demo - second file verification");
+			    // get another actual contents for real testing
+			    compareJsonResult(actualResult);
+		}else{							
+			logTAFWarning("Should this be what we want? '\n\t\t"+FormatHtmlReport.getHtmlPrintable(actualResult,100)+"..."+"'"	);
+		}
+
+		
+	}
+
+	//*************** Optional  ******************
+	// ******* main method for quick debugging ***
+	// *******************************************
+	
+	public static void main(String args){
+		KeywordExample debug = new KeywordExample();
+		debug.startBrowser("HtmlUnit");
+		debug.testAXRestAPI(debug.driver);
+	}
+}
 
