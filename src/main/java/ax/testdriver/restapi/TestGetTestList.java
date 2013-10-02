d301425ac0b6a54f2551eb981afbecf4b6e6e8be
karen-zou <karen_zou@acl.com>
2013-10-02 16:58:03 -0700

    add GetTestList and run RestAPI batches files

---
+++ src/main/java/ax/testdriver/restapi/TestGetTestList.java
@@ -0,0 +1,26 @@
+package ax.testdriver.restapi;
+
+import ax.lib.restapi.TestDriverExampleHelper;
+
+import com.acl.qa.taf.helper.Interface.TestDriverInterface;
+
+public class TestGetTestList  extends TestDriverExampleHelper implements TestDriverInterface {
+	
+    public static void main(String[] args){
+    	TestGetTestList test = new TestGetTestList();
+    	test.setMainScript(true);
+    	test.testMain(args);
+    }
+    
+    
+	@Override
+	public void testMain(Object[] args) 
+	{		
+		//startFromLine = 18; // 4-9
+		//endAtLine = 18;     //
+			
+		String poolFile = "testdata/ax/testdriver/restapi/GetTestList.xls";
+		exeTestCase(onInitialize(poolFile,getClass().getName()));
+	}
+	
+}
