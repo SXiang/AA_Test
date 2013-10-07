2729b4309af2ffe897a74ff22a599280bb6a1252
Steven Xiang <steven_xiang@acl.com>
2013-10-07 12:18:36 -0700

    --amend

--- src/main/resources/testdata/src/main/resources/taf/tool/StartIENode.bat
+++
@@ -1,8 +0,0 @@
-set PATH = "C:\Program Files\Java\jdk1.7.0_11\bin\";%PATH%
-
-set PATH = "C:\Selenium\";%PATH%
-
-cd C:\Selenium
-
-java -jar selenium-server-standalone-2.34.0.jar -role node -hub http://ramneet-7-64.acl.com:4444/grid/register -Dwebdriver.ie.driver=C:\Selenium\IEDriverServer.exe
-
