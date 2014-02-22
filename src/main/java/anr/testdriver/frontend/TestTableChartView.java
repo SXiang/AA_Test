46607aa4644edb22e46e414a2fea9f763516a327
Steven Xiang <steven_xiang@acl.com>
2014-02-21 17:19:11 -0800

    updated RunAnalyticWithNewParameterSet for icon/domain name changes

--- src/main/java/anr/testdriver/frontend/TestTableChartView.java
+++ src/main/java/anr/testdriver/frontend/TestTableChartView.java
@@ -30,9 +30,9 @@ public class TestTableChartView extends FrontendTestDriverHelper implements Test
 	{		
 
 		startFromLine = 2; // 4-9
-		endAtLine = 9;
+		//endAtLine = 9;
 			
-		String poolFile = "testdata/anr/testdriver/frontend/TestTableChartView.xls";
+		String poolFile = "testdata/ax/testdriver/frontend/TestTableChartView.xls";
 		exeTestCase(onInitialize(poolFile,getClass().getName()));		
 	}
 
